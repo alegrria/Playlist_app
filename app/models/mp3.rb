fe339f513b49253be933f514bc56c7f53b4da60d
Dende <alegrria@gmail.com>
2017-05-19 00:01:27 +0200

    Update README.md

--- app/models/mp3.rb
+++ app/models/mp3.rb
@@ -3,10 +3,10 @@ class Mp3 < ApplicationRecord
   require 'csv'
   def self.import(file)
 	encoding_options = {
-	  :invalid           => :replace,  # Replace invalid byte sequences
-	  :undef             => :replace,  # Replace anything not defined in ASCII
-	  :replace           => '',        # Use a blank for those replacements
-	  :universal_newline => true       # Always break lines with \n
+	  :invalid           => :replace, 
+	  :undef             => :replace,  
+	  :replace           => '',       
+	  :universal_newline => true     
 	}
 
   	CSV.foreach(file.path, headers: true, :header_converters => lambda { |h| h.downcase.gsub(' ', '_').encode(Encoding.find('ASCII'), encoding_options) } ) do |row|
