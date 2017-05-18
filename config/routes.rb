3021d68fa2cc3bbfaed9f4a3ac55b338e4f42e0f
Dende <alegrria@gmail.com>
2017-05-18 11:07:33 +0200

    Add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'user#index'
+ #get "search", controller: 'repo', action:'search'
 end
